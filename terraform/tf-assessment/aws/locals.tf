1fe9917e65c98e0c782a238b3dfc17e54025d805
Rishit Kumar <140222791+rishikkkkkk@users.noreply.github.com>
2026-05-17 15:35:01 +0000

    ffe

--- terraform/tf-assessment/aws/locals.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  app_name = "app"
-  enviroment_name = var.enviroment
-  version ="001"
-  final_name = "${local.app_name}-${local.enviroment_name}-${local.version}"
-}
--- terraform/tf-assessment/azure/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "enviroment" {
-  type = string
-  default = "dev"
-}
